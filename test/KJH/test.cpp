4f98bd1300212aa9957633f5bb99403610809673
gotka0211 <65711566+gotka0211@users.noreply.github.com>
2021-05-29 22:28:24 +0900

    Update test.cpp
    1. 게이트 구현
    2. 몸통 닿으면 사망
    3. 벽에 닿으면 사망
    4. 몸길이 3개 미만되면 사망

--- test/KJH/test.cpp
+++ test/KJH/test.cpp
@@ -3,7 +3,13 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+
 using namespace std;
+
+
+
+bool play= true; //게임 종료 플래그
+void fail(){play=false;} // 게임 종료 함수 
 
 
 // 각 오브젝트의 속성을 담는 부모 클래스
@@ -38,9 +44,11 @@ class Space :public Object
 class NormalWall : public Object
 {
     public:
+        bool isGate=false;
         NormalWall (int y, int x):Object(y,x,2,"NW"){}
 
         ~NormalWall(){}
+
 };
 // 딴딴벽
 class ImmueWall : public Object
@@ -82,8 +90,117 @@ class Apple : public Object
 
         ~Apple(){}
 };
+//게이트
+class Gate: public Object
+{
+    public:
+        Gate (int y, int x):Object(y,x,8,"GT"){}
+
+        ~Gate(){}
+};
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ게이트클래스ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+class GateNWall
+{
+    protected:
+        int height;
+        int width;
+        
+    public:
+        Object gate1;
+        Object gate2;
+        bool gateOpen=false; 
+
+        //출구를 구하기 위한 변수들
+        int exitOfGate[4][2] ={{-1,0},{0,1},{1,0},{0,-1}};
+        int idxForFind=0;
+
+        vector <Object> walls; //벽을 담는 벡터 
+
+        //생성자
+        GateNWall(Object**& m ,int h=21, int w= 21):height(h),width(w)
+        {
+            for (int j=0;j<h;j++)
+            {
+                for(int i=0;i<w;i++)
+                {
+                    if (m[j][i].getTN()==2)
+                    {
+                        walls.push_back(m[j][i]);
+                    }
+                }
+            }
+            selectGate(m);
+        }
+        //게이트 선택
+        void selectGate(Object** & m)
+        {
+            //먼저 현재 게이트는 벽으로 복구
+            restore(m);
+            // 게이트의 인덱스 뽑기 (2개)
+            int randIdx1,randIdx2;
+            do
+            {
+                srand((unsigned int)time(NULL));
+                randIdx1=rand()%(walls.size());
+                srand((unsigned int)time(NULL)*-1);
+                randIdx2=rand()%(walls.size());
+            } 
+            while (randIdx1==randIdx2);
+            
+            Object temp1 = walls[randIdx1];
+            Object temp2 = walls[randIdx2];
+
+            m[temp1.getY()][temp1.getX()] = Gate(temp1.getY(),temp1.getX());
+            m[temp2.getY()][temp2.getX()] = Gate(temp2.getY(),temp2.getX());
+
+            gate1 = m[temp1.getY()][temp1.getX()];
+            gate2 = m[temp2.getY()][temp2.getX()];
+
+            gateOpen=true;
+
+            // walls[randIdx1]= 
+        }
+        //게이트 되돌리기
+        void restore(Object**& m)
+        {
+            if (gateOpen)
+            {
+                m[gate1.getY()][gate1.getX()] = NormalWall(gate1.getY(),gate1.getX());
+                m[gate2.getY()][gate2.getX()] = NormalWall(gate2.getY(),gate2.getX());
+            }
+        }
+        //게이트 출구 좌표구하기
+        void setExit(Object**& m,Object& g,int& toY,int& toX)
+        {
+            int tempY;
+            int tempX;
+            // 원형 리스트의 시작 인덱스 구하기
+            for(int i=0;i<4;i++)
+            {
+                if (exitOfGate[i][0]== false && exitOfGate[i][1]) {idxForFind=i;break;} 
+            }
+
+            //시계방향으로 탐색
+            for (int i=0;i<4;i++)
+            {
+                tempY= g.getY()+exitOfGate[idxForFind][0],  
+                tempX= g.getX()+exitOfGate[idxForFind][1]; // 현재 게이트의 위치에서 시계방향으로 찾기
+                // 해당좌표가 빈공간이라면, 게이트의 출구임
+                if (0<=tempY &&tempY<height&& 0<tempX &&tempX< width) // 시계방향으로 확인하는 좌표가 맵 밖이 아니어야함
+                {
+                    if (m[tempY][tempX].getTN()==1){break;}  //만약 출구게이트의 사방이 벽으로 막혀있으면 죽음
+                }
+                ++idxForFind%=4;
+            }
+            int retToY = exitOfGate[idxForFind][0];
+            int retToX = exitOfGate[idxForFind][1];
+
+            toY=retToY;
+            toX=retToX;
+        }
+
+};
 //------------------------------ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ아이템클래스ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-// 아이템 ()
 class Item
 {
     protected:
@@ -128,7 +245,7 @@ class Item
             numItem--;
         }
         // 특정 아이템 제거  (뱀과 접촉시)
-        void eraseItem(int y, int x)
+        void eraseItem(int y,int x)
         {
             for (int i=0;i<numItem;i++)
             {
@@ -143,7 +260,6 @@ class Item
         }
 };
 //------------------------------ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ뱀클래스ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-// 뱀
 class Snake
 {
     int toX=-1;
@@ -152,6 +268,7 @@ class Snake
     
     public:
         int numBody=2;
+        int onGate =0;
         int  tailY, tailX;
         vector <Object> s;
         
@@ -164,8 +281,6 @@ class Snake
             Body body(tailY,++tailX);
             m[tailY][tailX]= body;
             s.push_back(body);
-            m[tailY][tailX]= body;
-            s.push_back(body);
 
             pushBody(m);
         }
@@ -173,6 +288,8 @@ class Snake
         void setToY(int y){toY=y;}
         void setToX(int x){toX=x;}
         
+        int getToY(){return toY;}
+        int getToX(){return toX;}
         //뱀 몸통 생성
         void pushBody(Object**& m)
         {
@@ -196,38 +313,46 @@ class Snake
             tailY = s[numBody-1].getY();
             tailX = s[numBody-1].getX();
         }
-        
-        // 뱀의 이동
-        void move(Object**& m)
+        // 뱀의 몸통 위치 설정(바로앞의 몸통의 위치로 이동함) (머리는 제외)
+        void setBodyPos()
         {
-            // 몸통이동
             for (int i=numBody-1; i>0; i--)
             {
                 s[i].setYX(s[i-1].getY(),s[i-1].getX());
             }
-            // 머리이동 
+        }
+        //머리위치설정(next값을 설정하고 그 값으로 이동)
+        void setHeadPos()
+        {
             nextY = s[0].getY()+toY;
             nextX = s[0].getX()+toX;
             s[0].setYX(nextY,nextX);   
-
+        }
+        // 뱀의 이동
+        void move(Object**& m,Item& item, GateNWall& gnw)
+        {
+            setBodyPos();
+            setHeadPos();
+            
             // 꼬리는 빈공간으로
             m[tailY][tailX]= Space(tailY,tailX); 
-            //그리고 꼬리 좌표 다시 2
+            //그리고 꼬리 좌표 다시 
             tailY = s[numBody-1].getY();
             tailX = s[numBody-1].getX(); 
-
+            
+            // 움직인 곳에 뭔가 있을때,
+            afterMove(m,item,gnw);            
+            
+            //변화 업뎃
             for (int i=0;i<numBody;i++)
             {
                 m[s[i].getY()][s[i].getX()]= s[i];
             }
         }
-
-        // 게임오버
-        void fail(){}
-
         //이동한 곳에 뭔가 있는 경우
-        void afterMove(Object**& m,Item& item)
+        void afterMove(Object**& m,Item& item,GateNWall& gnw)
         {
+            //아이템
             for (int i=0;i<item.numItem;i++)
             { 
                 Object obj = item.it[i];
@@ -250,26 +375,36 @@ class Snake
                         pushBody(m);
                         item.eraseItem(obj.getY(),obj.getX());
                     }
-                    else if (obj.getTN()==2) // 벽에 닿았을 때 실패
-                    {
-                        fail();
-                    }
-                    else if (obj.getTN()==2) // 빈 칸일 때
-                    {
-                        // 머리가 몸통에 닿았을 때 종료(꼬리방향 이동 포함)
-                        for (int i=1; i<s.size(); i++){
-                            // 몸통을 만나면 실패
-                            if ((s[i].getX() == s[0].getX()) && (s[i].getY() == s[0].getY())){
-                                fail();
-                            } 
-                        }
-                    }
                 }
+            }
+            // 아이템이 아닌경우 
+            Object obj = m[s[0].getY()][s[0].getX()]; //뱀 머리의 현재 위치에 있는 오브젝트
+            if (obj.getTN()==8) // 오브젝트가 게이트라면 
+            {
+                if (obj.getY() == gnw.gate1.getY() && obj.getX() == gnw.gate1.getX()) //오브젝트가 게이트1 일때  게이트 2로나옴
+                {
+                    
+                    gnw.setExit(m,gnw.gate2,toY,toX);
+                    s[0].setYX(gnw.gate2.getY()+toY,gnw.gate2.getX()+toX);
+                }
+                else //오브젝트가 게이트 2일때
+                {
+                    
+                    gnw.setExit(m,gnw.gate1,toY,toX);
+                    s[0].setYX(gnw.gate1.getY()+toY,gnw.gate1.getX()+toX);
+                }
+            }
+            else if (obj.getTN()==2) // 벽에 닿았을 때 실패
+            {
+                fail();
+            }
+            else if (obj.getTN()==4)  // 머리가 몸통에 닿았을 때 종료(꼬리방향 이동 포함)
+            {
+                fail();
             }
         }
 };
-//------------------------------ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ맵 클래스ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-// 맵
+//------------------------------ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ맵 클래스ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 class Map
 {
     int width;
@@ -366,7 +501,10 @@ void mapUpdate(Object** m, int h, int w)
                     attrset(COLOR_PAIR(7));
                     mvprintw(j, i*2, "00");
                     break;
-                
+                case 8: //게이트
+                    attrset(COLOR_PAIR(8));
+                    mvprintw(j, i*2, "00");
+                    break;
             }
         }
     }
@@ -377,25 +515,40 @@ void scoreUpdate(WINDOW* win,Snake& s)
 {
     mvwprintw(win, 7,2, "SCORE: %d/20     ",s.numBody);
 }
-// 시간의 흐름 
-// 레퍼런스 전달시 컴파일에러
-void timeUpdate(WINDOW* win,Snake *s,Object** *m,Item *i,int height, int width, bool* play)
+
+
+// 시간의 흐름             // 레퍼런스 전달시 컴파일에러
+void timeUpdate(WINDOW* win,Snake *s,Object** *m,Item *i,GateNWall *g,int height, int width)
 {
     double time =0;
     int tick= 100;  //몇초마다 화면 갱신 (뱀 이동 )할지 정함-  단위 ms 
-    int initCD = 4000; // 아이템이 나오는 시간- 단위 ms
-    double countdown = 0;
+    int initItemCD = 4000; // 아이템이 나오는 시간- 단위 ms
+    int initGateCD = 10000;
+    double ItemCD = 0;
+    double GateCD = 0;
     do {
-        //countdown이 0이 될때마다 아이템 생성 
-        if (countdown==0)  
+        // wclear(stdscr);
+
+        //ItemCD가 0이 될때마다 아이템 생성 
+        if (ItemCD==0)  
         {
-            if (time >=initCD*0.003 && i->numItem >=3) // 동시에 3개만 유지할수있도록. 근데 가끔씩 4개됨. 해결 필요 ****************************** 
+            if (time >=initItemCD*0.003 && i->numItem >=3) // 동시에 3개만 유지할수있도록. 근데 가끔씩 4개됨. 해결 필요 ****************************** 
             {
                 i->popItem(*m);
             }
             i->pushItem(*m);
-            countdown=initCD;
+            ItemCD=initItemCD;
         }
+        // GateCD가 0이 될때마다 게이트 생성
+        if (GateCD==0)
+        {
+            g->selectGate(*m); //게이트 생성
+
+            GateCD=initGateCD;
+
+            mvwprintw(win, 11,2, "(%d,%d)  ,  (%d,%d)   ",g->gate1.getY(),g->gate1.getX(),g->gate2.getY(),g->gate2.getX()); //디버깅용 포탈위치 출력
+        }
+        
         
         // 시간 업데이트
         mvwprintw(win, 8,2, "TIME: %1.1f sec  ",time);
@@ -404,20 +557,21 @@ void timeUpdate(WINDOW* win,Snake *s,Object** *m,Item *i,int height, int width, 
         wrefresh(win);
 
         // 뱀 움직임 업데이트
-        s->move(*m);
-        s->afterMove(*m,*i);
+        s->move(*m,*i,*g);
         scoreUpdate(win,*s);// 점수변화
         mapUpdate(*m,21,21);
+        mvwprintw(win, 13,2, "   ( %d,%d) %d  ", s->s[0].getY(),s->s[0].getX(),s->s[0].getTN()); // 디버깅용 뱀 머리 위치 출력
         // refresh();   
-        wrefresh(stdscr);  //refresh쓸때보다 화면이 덜 깨짐 (기분탓일 수도 있음) ******************************
+        // wrefresh(stdscr);  //refresh쓸때보다 화면이 덜 깨짐 (기분탓일 수도 있음) ******************************
 
         napms(tick); //sleep과 같은 기능
-        countdown-=tick;
-
-    } while (*play=true);
+        ItemCD-=tick;
+        GateCD-=tick;
+    } while (play==true);
 }
+
+
 //------------------------------ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ메인함수ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-//
 int main()
 {
     // 겜초기화
@@ -428,6 +582,8 @@ int main()
     Item item(map.m,map.getHeight(),map.getWidth());
     // 뱀 초기화
     Snake snake(map.m,map.getHeight(),map.getWidth());
+    // 게이트 &월 초기화
+    GateNWall gnw(map.m,map.getHeight(),map.getWidth());
 
     initscr(); 
     
@@ -442,7 +598,8 @@ int main()
     init_color(12, 400, 400, 400); // 일반벽 색 (회색);
     init_color(13, 0,0,0);// 무적 벽(검은색);
     init_color(14,1000,700,0 ); // 몸색 (노란색)
-    init_color(15, 1000, 400,0); // 머리색 (오렌지);
+    init_color(15, 1000, 400,0); // 머리색 (오렌지;
+    init_color(18,1000,100,500); // 게이트색(핑크)
     // 만든 색상 적용
     init_pair(1, 11, 11); //빈공간 색
     init_pair(2,12,12);                     // 일반 벽 색
@@ -451,11 +608,12 @@ int main()
     init_pair(5,  15, 15); // 뱀 머리 색
     init_pair(6, COLOR_GREEN, COLOR_GREEN); // 독색
     init_pair(7, COLOR_RED, COLOR_RED); //사과색
+    init_pair(8, 18,18); //게이트색
     init_pair(10, COLOR_BLACK, COLOR_WHITE); //상태창 색
     // init_pair(10, COLOR_GREEN, COLOR_MAGENTA);
 
     //맵 그래픽 생성
-    mapUpdate(map.m,map.getHeight(),map.getWidth());
+    // mapUpdate(map.m,map.getHeight(),map.getWidth());
     
     refresh();
     //상태창
@@ -477,12 +635,12 @@ int main()
     mvwprintw(win, 9,2, "num of items : %d     ",item.numItem);
     wrefresh(win);
     
-    bool play=true; //게임 종료 플래그. 아직 작동 안됨 **********************************************************
-    thread t(timeUpdate,win,&snake, &map.m, &item, map.getHeight(),map.getWidth(),&play); // 스레드 생성
+    
+    thread t(timeUpdate,win,&snake, &map.m, &item,&gnw, map.getHeight(),map.getWidth()); // 스레드 생성
 
     /////////////////////////////////////////////
     // 키입력 및 게임 메인 루프. 종료조건도 입력해야함******************************************************
-    attrset(COLOR_PAIR(4));
+    // attrset(COLOR_PAIR(4));
     int input;
     keypad(stdscr, true); //특수키 입력받기 활성화
     while(true)
@@ -491,22 +649,29 @@ int main()
         switch(input)
         {
             case KEY_UP:
-                // if(snake.direction == 4) endwin();
+                if(snake.getToY() == 1 && snake.getToX() ==0) {fail();} //진행방향과 반대키 아직 제대로 동작안함 쓰레드 끊어야함
+                
                 snake.setToY(-1);
                 snake.setToX(0);
                 break;
+
             case KEY_DOWN:
-                // if(snake.direction == 3) endwin();
+                if(snake.getToY() == -1 && snake.getToX() ==0) {fail();}
+                
                 snake.setToY(1);
                 snake.setToX(0);
                 break;
+                
             case KEY_LEFT:
-                // if(snake.direction == 2) endwin();
+                if(snake.getToY()== 0 && snake.getToX() ==1) {fail();}
+                
                 snake.setToY(0);
                 snake.setToX(-1);
                 break;
+
             case KEY_RIGHT:
-                // if(snake.direction == 1) endwin();
+                if(snake.getToY()== 0 && snake.getToX() ==-1) {fail();}
+
                 snake.setToY(0);
                 snake.setToX(1);
                 break;
@@ -514,14 +679,15 @@ int main()
 
 
         //디버깅용 입력
+
 	    // 한칸씩 이동
-        if(input == 'm')
-        {
-            snake.move(map.m);
-            snake.afterMove(map.m,item);
-            scoreUpdate(win,snake);// 점수변화
-            refresh();
-        }   
+        // if(input == 'm')
+        // {
+        //     snake.move(map.m);
+        //     snake.afterMove(map.m,item,gnw);
+        //     scoreUpdate(win,snake);// 점수변화
+        //     refresh();
+        // }   
         // 몸통추가
         if (input == '1')
         {
@@ -546,68 +712,17 @@ int main()
         if (input == 'q')
         {
             play = false;
-            mvwprintw(win, 11,2, "false 됨 게임중단해야함");
+            mvwprintw(win, 15,2, "false ");
             wrefresh(win);
         }
 
         //업뎃한거 적용 
-        mapUpdate(map.m,map.getHeight(),map.getWidth());
-        refresh();
-        wrefresh(win); 
+        // mapUpdate(map.m,map.getHeight(),map.getWidth());
+        // refresh();
+        // wrefresh(win); 
     }
     if(t.joinable()){t.join();} // 다른 스레드가 먼저 끝나기를 기다림. 
     endwin(); // 화면 끔
-
-
-    
-    
-
-    // map.print();
-
-    // while (true)
-    // {
-    //     char c;
-    //     cin >>c;
-
-    //     if (c=='a'){snake.setToY(0);snake.setToX(-1);snake.move(map.m);map.print();}
-    //     else if(c=='d'){snake.setToY(0);snake.setToX(1);snake.move(map.m);map.print();}
-    //     else if(c=='w'){snake.setToY(-1);snake.setToX(0);snake.move(map.m);map.print();}
-    //     else if(c=='s'){snake.setToY(1);snake.setToX(0);snake.move(map.m);map.print();}
-
-    //     else if(c=='1'){snake.popBody(map.m);map.print();}
-    //     else if(c=='2'){snake.pushBody(map.m);map.print();}
-    // }
-
-    // snake.move(map.m);
-
-
-    // map.print();
-    // for(int i=0;i<3;i++)
-    // {
-    //     snake.move(map.m);
-    //     map.print();
-    //     cout << snake.tailY <<", "<<snake.tailX<< endl;
-    // }
-    
-    // snake.popBody(map.m);   
-    // cout << snake.tailY <<", "<<snake.tailX<< endl;
-    // map.print();
-    // cout << snake.tailY <<", "<<snake.tailX<< endl;
-    // snake.move(map.m);
-    // map.print();
-    // cout << snake.tailY <<", "<<snake.tailX<< endl;
-    
-    // snake.move(map.m);
-    // map.print();
-    // cout << snake.tailY <<", "<<snake.tailX<< endl;
-
-    // snake.pushBody(map.m);
-    // map.print();
-    // cout << snake.tailY <<", "<<snake.tailX<< endl;
-
-    // snake.move(map.m);
-    // map.print();
-    // cout << snake.tailY <<", "<<snake.tailX<< endl;
 
     return 0;
 }
