15965d54f0ce2302dbab4fe87cd1146d0b304505
gotka0211 <65711566+gotka0211@users.noreply.github.com>
2021-05-30 16:27:53 +0900

    Update test.cpp
    [한거]
    1. 스테이지 구현
    2. 사망 구현  (fail()함수)
    3. 게이트 구현
    [해야 함]
    1. 실패화면 (재시작할 수 있게, or 그냥 확인버튼 -지금은 그냥 화면이 멈춤)
    2.코드 정리

--- test/KJH/test.cpp
+++ test/KJH/test.cpp
@@ -11,6 +11,161 @@ using namespace std;
 bool play= true; //게임 종료 플래그
 void fail(){play=false;} // 게임 종료 함수 
 
+class Score
+{
+public:
+    int stageNum;
+    int scoreBodyLen;
+    int scoreGateUse;
+    int scoreTime;
+
+    Score(int stageNum = 1, int scoreBodyLen = 0, int scoreGateUse = 0, int scoreTime = 0)
+    :stageNum(stageNum), scoreBodyLen(scoreBodyLen), scoreGateUse(scoreGateUse), scoreTime(scoreTime){}
+
+    bool LevelUp()
+    {
+        switch(stageNum)
+        {
+            case 1:
+            if(scoreBodyLen >= 3 &&  scoreGateUse >= 0 && scoreTime > 0)
+            {
+                stageNum++;
+                clear();
+                WINDOW *loadingWin = subwin(stdscr, 5, 60, 10, 10);
+                init_pair(21, COLOR_WHITE, COLOR_GREEN);
+                box(loadingWin, 0,0);
+                attron(COLOR_PAIR(21));
+                wbkgd(loadingWin, COLOR_PAIR(21));
+                refresh();
+
+                init_pair(20, COLOR_RED, COLOR_GREEN);
+                attron(COLOR_PAIR(20));
+                mvprintw(11, 24, "Are you ready for the next stage?");
+                attron(A_BLINK);
+                attron(COLOR_PAIR(21));
+                mvprintw(13, 32, "press 'r' key");
+                return true;
+            }
+            case 2:
+            if(scoreBodyLen >= 5 &&  scoreGateUse >= 0 && scoreTime > 0)
+            {
+                stageNum++;
+                clear();
+                WINDOW *loadingWin = subwin(stdscr, 5, 60, 10, 10);
+                init_pair(21, COLOR_WHITE, COLOR_GREEN);
+                box(loadingWin, 0,0);
+                attron(COLOR_PAIR(21));
+                wbkgd(loadingWin, COLOR_PAIR(21));
+                refresh();
+
+                init_pair(20, COLOR_RED, COLOR_GREEN);
+                attron(COLOR_PAIR(20));
+                mvprintw(11, 24, "Are you ready for the next stage?");
+                attron(A_BLINK);
+                attron(COLOR_PAIR(21));
+                mvprintw(13, 32, "press 'r' key");
+                return true;
+            }
+            case 3:
+            if(scoreBodyLen >= 10 &&  scoreGateUse >= 0 && scoreTime > 0)
+            {
+                stageNum++;
+                clear();
+
+                init_pair(20, COLOR_RED, COLOR_RED);
+                attron(COLOR_PAIR(20));
+                int x = 5; int y = 11;
+                mvprintw(x, y+2, "00");
+                mvprintw(x, y+4, "00");
+                mvprintw(x, y+6, "00");
+                mvprintw(x, y+8, "00");
+                mvprintw(x+1, y, "00");
+                mvprintw(x+2, y, "00");
+                mvprintw(x+3, y, "00");
+                mvprintw(x+4, y, "00");
+                mvprintw(x+5, y, "00");
+                mvprintw(x+6, y+2, "00");
+                mvprintw(x+6, y+4, "00");
+                mvprintw(x+6, y+6, "00");
+                mvprintw(x+6, y+8, "00");
+
+                mvprintw(x, y+12, "00");
+                mvprintw(x+1, y+12, "00");
+                mvprintw(x+2, y+12, "00");
+                mvprintw(x+3, y+12, "00");
+                mvprintw(x+4, y+12, "00");
+                mvprintw(x+5, y+12, "00");
+                mvprintw(x+6, y+12, "00");
+
+                mvprintw(x+1, y+16, "00");
+                mvprintw(x+2, y+16, "00");
+                mvprintw(x+3, y+16, "00");
+                mvprintw(x+4, y+16, "00");
+                mvprintw(x+5, y+16, "00");
+                mvprintw(x, y+18, "00");
+                mvprintw(x, y+20, "00");
+                mvprintw(x, y+22, "00");
+                mvprintw(x, y+24, "00");
+                mvprintw(x+3, y+18, "00");
+                mvprintw(x+3, y+20, "00");
+                mvprintw(x+3, y+22, "00");
+                mvprintw(x+3, y+24, "00");
+                mvprintw(x+6, y+18, "00");
+                mvprintw(x+6, y+20, "00");
+                mvprintw(x+6, y+22, "00");
+                mvprintw(x+6, y+24, "00");
+                mvprintw(x+5, y+26, "00");
+                mvprintw(x+1, y+26, "00");
+                mvprintw(x+2, y+26, "00");
+
+                mvprintw(x+2, y+30, "00");
+                mvprintw(x+3, y+30, "00");
+                mvprintw(x+4, y+30, "00");
+                mvprintw(x+1, y+32, "00");
+                mvprintw(x+5, y+32, "00");
+                mvprintw(x, y+34, "00");
+                mvprintw(x, y+36, "00");
+                mvprintw(x, y+38, "00");
+                mvprintw(x+6, y+34, "00");
+                mvprintw(x+6, y+36, "00");
+                mvprintw(x+5, y+38, "00");
+                mvprintw(x+4, y+40, "00");
+                mvprintw(x+1, y+40, "00");
+                mvprintw(x+2, y+42, "00");
+                mvprintw(x+3, y+42, "00");
+                mvprintw(x+4, y+42, "00");
+                mvprintw(x+5, y+42, "00");
+                mvprintw(x+6, y+44, "00");
+
+                mvprintw(x, y+48, "00");
+                mvprintw(x+1, y+48, "00");
+                mvprintw(x+2, y+48, "00");
+                mvprintw(x+3, y+48, "00");
+                mvprintw(x+4, y+48, "00");
+                mvprintw(x+5, y+48, "00");
+                mvprintw(x+6, y+48, "00");
+                mvprintw(x+2, y+50, "00");
+                mvprintw(x+1, y+52, "00");
+                mvprintw(x, y+54, "00");
+                mvprintw(x, y+56, "00");
+
+                WINDOW *startWin = subwin(stdscr, 5, 60, 15, 10);
+                init_pair(1, COLOR_WHITE, COLOR_GREEN);
+                box(startWin, 0,0);
+                attron(COLOR_PAIR(1));
+                wbkgd(startWin, COLOR_PAIR(1));
+                refresh();
+
+                attron(A_BLINK);
+                mvprintw(17, 22, "Congratulations! cleared all stages");
+                return true;
+            }
+            break;
+        }
+        return false;
+    }
+};
+
 
 // 각 오브젝트의 속성을 담는 부모 클래스
 class Object
@@ -30,6 +185,8 @@ class Object
         void setYX(int y,int x){this->y=y;this->x=x;}
 
         int getTN(){return typeNum;}
+
+        void setTN(int typeNum) {this->typeNum = typeNum;}
 
 };
 // 빈공간
@@ -212,6 +369,12 @@ class Item
 
         Item(Object**& m, int h = 21, int w = 21): height(h), width(w) { }
 
+        void Init(Object**& m)
+        {
+            while(numItem > 0) popItem(m);
+        }
+
+
         // 아이템 생성 (시간마다)
         void pushItem(Object**& m)
         {
@@ -283,6 +446,18 @@ class Snake
             s.push_back(body);
 
             pushBody(m);
+        }
+
+        void Init(Object**& m)
+        {
+            while(numBody > 3) popBody(m);
+            m[s[0].getY()][s[0].getX()].setTN(1);
+            m[s[1].getY()][s[1].getX()].setTN(1);
+            m[s[2].getY()][s[2].getX()].setTN(1);
+
+            s[0].setYX(10, 20);
+            s[1].setYX(10, 21);
+            s[2].setYX(10, 22);
         }
 
         void setToY(int y){toY=y;}
@@ -441,19 +616,21 @@ class Map
         int getWidth(){return width;}
         int getStageNum(){return stageNum;}
 
-        // 초기화 잘됐는지 출력 (이젠 안씀)
-        void print()
+
+        void makeHorizontal(int height)
         {
-            
-            for (int j=0;j<height;j++)
+            for(int i = 3; i < width-3; i++)
             {
-                for(int i=0;i<width;i++)
-                {
-                    cout<< m[j][i].str <<" ";
-                }
-                cout<<endl;
+                m[height][i] = NormalWall(height, i);
             }
-            cout <<"stage : "<<stageNum<<endl;
+        }
+
+        void makeVertical(int width)
+        {
+            for(int i = 3; i < height-3; i++)
+            {
+                m[i][width] = NormalWall(i, width);
+            }
         }
 };
 
@@ -518,66 +695,261 @@ void scoreUpdate(WINDOW* win,Snake& s)
 
 
 // 시간의 흐름             // 레퍼런스 전달시 컴파일에러
-void timeUpdate(WINDOW* win,Snake *s,Object** *m,Item *i,GateNWall *g,int height, int width)
+void timeUpdate(Snake *s,Object** *m,Item *i,GateNWall *g,int height, int width, Score *score, Map *map)
 {
-    double time =0;
-    int tick= 100;  //몇초마다 화면 갱신 (뱀 이동 )할지 정함-  단위 ms 
-    int initItemCD = 4000; // 아이템이 나오는 시간- 단위 ms
-    int initGateCD = 10000;
-    double ItemCD = 0;
-    double GateCD = 0;
-    do {
-        // wclear(stdscr);
+    for(int a=0;a <4; a++)
+    {
+        //상태창
+        WINDOW *win;
+        win = newwin(map->getHeight(),30,0,map->getWidth()*2);
+        wbkgd(win, COLOR_PAIR(10));
+        box(win, 0,0);
 
-        //ItemCD가 0이 될때마다 아이템 생성 
-        if (ItemCD==0)  
-        {
-            if (time >=initItemCD*0.003 && i->numItem >=3) // 동시에 3개만 유지할수있도록. 근데 가끔씩 4개됨. 해결 필요 ****************************** 
-            {
-                i->popItem(*m);
-            }
-            i->pushItem(*m);
-            ItemCD=initItemCD;
-        }
-        // GateCD가 0이 될때마다 게이트 생성
-        if (GateCD==0)
-        {
-            g->selectGate(*m); //게이트 생성
-
-            GateCD=initGateCD;
-
-            mvwprintw(win, 11,2, "(%d,%d)  ,  (%d,%d)   ",g->gate1.getY(),g->gate1.getX(),g->gate2.getY(),g->gate2.getX()); //디버깅용 포탈위치 출력
-        }
-        
-        
-        // 시간 업데이트
-        mvwprintw(win, 8,2, "TIME: %1.1f sec  ",time);
+        wattron(win,A_BOLD); // 제목 강조
+        mvwprintw(win, 1,2, "SNAKE GAME");
+        wattroff(win,A_BOLD);
+        mvwprintw(win, 4,2, "STATUS:");
+        mvwprintw(win, 6,2, "STAGE: %d",score->stageNum);
+        mvwprintw(win, 7,2, "SCORE: %d/20",s->numBody);
+        mvwprintw(win, 8,2, "TIME: %d sec", 0);
         mvwprintw(win, 9,2, "num of items : %d     ",i->numItem);
-        time+=tick*0.001;
-        wrefresh(win);
+        mvwprintw(win, 10,5, "- score");
+        mvwprintw(win, 11,5, "+ score");
+        wattron(win,COLOR_PAIR(6));
+        mvwprintw(win, 10,2, "00");
+        wattroff(win,COLOR_PAIR(6));
+        wattron(win,COLOR_PAIR(7));
+        mvwprintw(win, 11,2, "00");
+        wattroff(win,COLOR_PAIR(7));
+        
+        double time =0;
+        int tick= 100;  //몇초마다 화면 갱신 (뱀 이동 )할지 정함-  단위 ms 
+        int initItemCD = 4000; // 아이템이 나오는 시간- 단위 ms
+        int initGateCD = 10000;
+        double ItemCD = 0;
+        double GateCD = 0;
 
-        // 뱀 움직임 업데이트
-        s->move(*m,*i,*g);
-        scoreUpdate(win,*s);// 점수변화
-        mapUpdate(*m,21,21);
-        mvwprintw(win, 13,2, "   ( %d,%d) %d  ", s->s[0].getY(),s->s[0].getX(),s->s[0].getTN()); // 디버깅용 뱀 머리 위치 출력
-        // refresh();   
-        // wrefresh(stdscr);  //refresh쓸때보다 화면이 덜 깨짐 (기분탓일 수도 있음) ******************************
+        if(a > 0)
+            {
+                s->Init(map->m);
+                i->Init(map->m);
+            }
 
-        napms(tick); //sleep과 같은 기능
-        ItemCD-=tick;
-        GateCD-=tick;
-    } while (play==true);
+            if(score->stageNum == 2) map->makeVertical(10);
+            if(score->stageNum == 3) map->makeHorizontal(15);
+
+        do {
+            box(win, 0,0);
+
+            wattron(win,A_BOLD); // 제목 강조
+            mvwprintw(win, 1,2, "SNAKE GAME");
+            wattroff(win,A_BOLD);
+            mvwprintw(win, 4,2, "STATUS:");
+            mvwprintw(win, 6,2, "STAGE: %d",score->stageNum);
+            mvwprintw(win, 9,2, "num of items : %d     ",i->numItem);
+            // wclear(stdscr);
+
+            //ItemCD가 0이 될때마다 아이템 생성 
+            if (ItemCD==0)  
+            {
+                if (time >=initItemCD*0.003 && i->numItem >=3) // 동시에 3개만 유지할수있도록. 근데 가끔씩 4개됨. 해결 필요 ****************************** 
+                {
+                    i->popItem(*m);
+                }
+                i->pushItem(*m);
+                ItemCD=initItemCD;
+            }
+            // GateCD가 0이 될때마다 게이트 생성
+            if (GateCD==0)
+            {
+                g->selectGate(*m); //게이트 생성
+
+                GateCD=initGateCD;
+
+                mvwprintw(win, 11,2, "(%d,%d)  ,  (%d,%d)   ",g->gate1.getY(),g->gate1.getX(),g->gate2.getY(),g->gate2.getX()); //디버깅용 포탈위치 출력
+            }
+            
+            // 점수업데이트
+            if(score->stageNum == 1) mvwprintw(win, 7,2, "SCORE: %d/3     ",s->numBody-3);
+            else if(score->stageNum == 2) mvwprintw(win, 7,2, "SCORE: %d/5     ",s->numBody-3);
+            else if(score->stageNum == 3) mvwprintw(win, 7,2, "SCORE: %d/10     ",s->numBody-3);
+            score->scoreTime = time;
+            score->scoreBodyLen = s->numBody-3;
+            
+
+            // 시간 업데이트
+            mvwprintw(win, 8,2, "TIME: %1.1f sec  ",time);
+            mvwprintw(win, 9,2, "num of items : %d     ",i->numItem);
+            time+=tick*0.001;
+            wrefresh(win);
+
+            // 뱀 움직임 업데이트
+            s->move(*m,*i,*g);
+            scoreUpdate(win,*s);// 점수변화
+            mapUpdate(*m,24,30);
+            mvwprintw(win, 13,2, "   ( %d,%d) %d  ", s->s[0].getY(),s->s[0].getX(),s->s[0].getTN()); // 디버깅용 뱀 머리 위치 출력
+            // refresh();   
+            // wrefresh(stdscr);  //refresh쓸때보다 화면이 덜 깨짐 (기분탓일 수도 있음) ******************************
+
+            napms(tick); //sleep과 같은 기능
+            ItemCD-=tick;
+            GateCD-=tick;
+
+            bool stageEnd = false;
+                if(score->LevelUp() == true)
+                {
+
+                    while(1)
+                    {
+                        int input = getch();
+                        if(input == 'r')
+                        {
+                            stageEnd = true;
+                            break;
+                        } 
+                    }
+                }
+                if(stageEnd == true)
+                {
+                    clear();
+                    break;
+                }
+        } while (play==true);
+    }
 }
 
 
 //------------------------------ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ메인함수ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 int main()
 {
+    initscr();
+
+    noecho(); // 입력을 자동으로 화면에 출력하지 않도록 합니다.
+    curs_set(FALSE); // cursor를 보이지 않게 합니다. 
+    start_color();
+
+    int x = 4;
+    int y = 10;
+
+    int b = 33;
+    mvprintw(12, b, "G");
+    mvprintw(12, b+4, "A");
+    mvprintw(12, b+8, "M");
+    mvprintw(12, b+12, "E");
+
+    init_color(14,1000,700,0 ); // 몸색 (노란색)
+    init_color(15, 1000, 400,0); // 머리색 (오렌지)
+    init_pair(4, 14, 14); //뱀 몸통 색
+    init_pair(5,  15, 15); // 뱀 머리 색
+    //s
+    attron(COLOR_PAIR(5));
+    mvprintw(x+0, y+2, "00");
+    mvprintw(x+0, y+4, "00");
+    mvprintw(x+0, y+6, "00");
+    mvprintw(x+1, y, "00");
+    mvprintw(x+2, y, "00");
+    mvprintw(x+3, y+2, "00");
+    mvprintw(x+3, y+4, "00");
+    mvprintw(x+3, y+6, "00");
+    mvprintw(x+4, y+8, "00");
+    mvprintw(x+5, y+8, "00");
+    mvprintw(x+6, y+2, "00");
+    mvprintw(x+6, y+4, "00");
+    mvprintw(x+6, y+6, "00");
+    //nake
+    attron(COLOR_PAIR(4));
+    mvprintw(x, y+12, "00");
+    mvprintw(x+1, y+12, "00");
+    mvprintw(x+2, y+12, "00");
+    mvprintw(x+3, y+12, "00");
+    mvprintw(x+4, y+12, "00");
+    mvprintw(x+5, y+12, "00");
+    mvprintw(x+6, y+12, "00");
+    mvprintw(x+2, y+14, "00");
+    mvprintw(x+3, y+16, "00");
+    mvprintw(x+4, y+18, "00");
+    mvprintw(x, y+20, "00");
+    mvprintw(x+1, y+20, "00");
+    mvprintw(x+2, y+20, "00");
+    mvprintw(x+3, y+20, "00");
+    mvprintw(x+4, y+20, "00");
+    mvprintw(x+5, y+20, "00");
+    mvprintw(x+6, y+20, "00");
+
+    mvprintw(x+3, y+24, "00");
+    mvprintw(x+4, y+24, "00");
+    mvprintw(x+5, y+24, "00");
+    mvprintw(x+6, y+24, "00");
+    mvprintw(x+4, y+26, "00");
+    mvprintw(x+4, y+28, "00");
+    mvprintw(x+4, y+30, "00");
+    mvprintw(x+3, y+32, "00");
+    mvprintw(x+4, y+32, "00");
+    mvprintw(x+5, y+32, "00");
+    mvprintw(x+6, y+32, "00");
+    mvprintw(x, y+28, "00");
+    mvprintw(x+1, y+26, "00");
+    mvprintw(x+2, y+26, "00");
+    mvprintw(x+1, y+30, "00");
+    mvprintw(x+2, y+30, "00");
+
+    mvprintw(x, y+36, "00");
+    mvprintw(x+1, y+36, "00");
+    mvprintw(x+2, y+36, "00");
+    mvprintw(x+3, y+36, "00");
+    mvprintw(x+4, y+36, "00");
+    mvprintw(x+5, y+36, "00");
+    mvprintw(x+6, y+36, "00");
+    mvprintw(x+3, y+38, "00");
+    mvprintw(x+2, y+40, "00");
+    mvprintw(x+4, y+40, "00");
+    mvprintw(x+1, y+42, "00");
+    mvprintw(x+5, y+42, "00");
+    mvprintw(x, y+44, "00");
+    mvprintw(x+6, y+44, "00");
+
+    mvprintw(x, y+48, "00");
+    mvprintw(x+1, y+48, "00");
+    mvprintw(x+2, y+48, "00");
+    mvprintw(x+3, y+48, "00");
+    mvprintw(x+4, y+48, "00");
+    mvprintw(x+5, y+48, "00");
+    mvprintw(x+6, y+48, "00");
+    mvprintw(x, y+50, "00");
+    mvprintw(x, y+52, "00");
+    mvprintw(x, y+54, "00");
+    mvprintw(x, y+56, "00");
+    mvprintw(x+3, y+50, "00");
+    mvprintw(x+3, y+52, "00");
+    mvprintw(x+3, y+54, "00");
+    mvprintw(x+3, y+56, "00");
+    mvprintw(x+6, y+50, "00");
+    mvprintw(x+6, y+52, "00");
+    mvprintw(x+6, y+54, "00");
+    mvprintw(x+6, y+56, "00");
+    
+
+    WINDOW *startWin = subwin(stdscr, 5, 60, 15, 10);
+    init_pair(1, COLOR_WHITE, COLOR_GREEN);
+    box(startWin, 0,0);
+    attron(COLOR_PAIR(1));
+    wbkgd(startWin, COLOR_PAIR(1));
+    refresh();
+
+    attron(A_BLINK);
+    mvprintw(17, 32, "press 's' key");
+    // 겜초기화
+    while(1){
+        int input = getch();
+        if(input == 's') break;
+    }
+
+    clear();
+    
     // 겜초기화
 
     // 맵 초기화
-    Map map(21,21,1);
+    Map map(30,24,1);
     //아이템 리스트 초기화 
     Item item(map.m,map.getHeight(),map.getWidth());
     // 뱀 초기화
@@ -585,10 +957,8 @@ int main()
     // 게이트 &월 초기화
     GateNWall gnw(map.m,map.getHeight(),map.getWidth());
 
-    initscr(); 
-    
-    noecho(); // 입력을 자동으로 화면에 출력하지 않도록 합니다.
-    curs_set(FALSE); // cursor를 보이지 않게 합니다. 
+    Score score;
+
     // attron(A_CHARTEXT);
 
     //팔레트
@@ -613,30 +983,13 @@ int main()
     // init_pair(10, COLOR_GREEN, COLOR_MAGENTA);
 
     //맵 그래픽 생성
-    // mapUpdate(map.m,map.getHeight(),map.getWidth());
     
-    refresh();
-    //상태창
-    WINDOW *win;
-    win = newwin(map.getHeight(),30,0,map.getWidth()*2);
-    wbkgd(win, COLOR_PAIR(10));
-    box(win, 0,0);
+    // refresh();
+    
 
-    // wattron(win, COLOR_PAIR(10));
-
-    int time=0;
-    wattron(win,A_BOLD); // 제목 강조
-    mvwprintw(win, 1,2, "SNAKE GAME");
-    wattroff(win,A_BOLD);
-    mvwprintw(win, 4,2, "STATUS:");
-    mvwprintw(win, 6,2, "STAGE: %d",map.getStageNum());
-    mvwprintw(win, 7,2, "SCORE: %d/20",snake.numBody);
-    mvwprintw(win, 8,2, "TIME: %d sec",time);
-    mvwprintw(win, 9,2, "num of items : %d     ",item.numItem);
-    wrefresh(win);
+    int time = 0;
     
-    
-    thread t(timeUpdate,win,&snake, &map.m, &item,&gnw, map.getHeight(),map.getWidth()); // 스레드 생성
+    thread t(timeUpdate,&snake, &map.m, &item,&gnw, map.getHeight(),map.getWidth(),&score, &map); // 스레드 생성
 
     /////////////////////////////////////////////
     // 키입력 및 게임 메인 루프. 종료조건도 입력해야함******************************************************
@@ -712,8 +1065,6 @@ int main()
         if (input == 'q')
         {
             play = false;
-            mvwprintw(win, 15,2, "false ");
-            wrefresh(win);
         }
 
         //업뎃한거 적용 
